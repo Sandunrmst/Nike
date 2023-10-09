ef548da90ea39a0778932ec73d2cd0f5acef037b
sandunrmst <sandunalugc@gmail.com>
2023-10-09 19:20:22 +0530

    Complete Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,13 +7,13 @@ import { useState } from "react"
 
 const Hero = () => {
  
-  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+  const [bigShoeImg, setBigShoeImg] = useState("")
 
   return (
    <section id="home" className="w-full flex p-2 xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
     <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
       <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[80px] font-bold">
         <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span><br/>
         <span className="text-coral-red inline-block mt-2">Nike</span> Shoes
       </h1>
@@ -34,7 +34,7 @@ const Hero = () => {
 
     <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
       
-      <img src={bigShoeImg} alt="Shoe Collection" width={610} height={500} className="object-contain relative z-10"/>
+      <img src={bigShoeImg || bigShoe1} alt="Shoe Collection" width={610} height={500} className="object-contain relative z-10"/>
       
       <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
         {shoes.map((shoe) => (
